37aea567cc376bbe7a930045211e96711513ba51
morganaustad <morganaustad@gmail.com>
2026-02-11 16:05:45 -0700

    Add temporary game page functionality

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export function Store() {
-    
+    const [game, setGame] = React.useState('');
+
+    // Edit function as needed when API is linked
+    function gameChange(e) {
+        setGame(e.target.value);
+    }
+
     return (
         <main className="border px-3 py-3">
-            <h1>Game Name</h1>
+            <h1>{game ? game : "Game Missing"}</h1>
             <p>Game description</p>
             <p>(This page is for steam store api and information of the game.)</p>
+            <p>(After we link the API or however that works, then I'll be able to link the game name to the rest of the app [much of the app has conditional logic behind the game name being present in localstorage, such as the title here])</p>
             <img src="https://playlegend.net/wp-content/uploads/2021/11/lgnd_fp_gd-dm.jpg" width="100%" />
         </main>
     );
