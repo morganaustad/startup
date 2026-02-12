cf4039935d3a63fa513360abbc0e9403a3fe1304
morganaustad <morganaustad@gmail.com>
2026-02-11 17:14:00 -0700

    Adjust feature description box

--- src/features/features.jsx
+++ src/features/features.jsx
@@ -3,21 +3,12 @@ import { BrowserRouter, NavLink, Routes, Route, useNavigate } from 'react-router
 
 export function Features({user, game}) {
     const [msg, setMsg] = React.useState('...Listening');
-    const [msg2, setMsg2] = React.useState('...Listening');
 
     React.useEffect(() => {
         setInterval(() => {
             const names = ['Morgan', 'Lee', 'Jay', 'TesterPoo131'];
             const randomName = names[Math.floor(Math.random() * names.length)];
             setMsg(`${randomName}'s feature suggestion:`);
-        }, 1500);
-    });
-
-    React.useEffect(() => {
-        setInterval(() => {
-            const names = ['Fred', 'Hank', 'Gary', 'Sally'];
-            const randomName = names[Math.floor(Math.random() * names.length)];
-            setMsg2(`${randomName}'s feature suggestion:`);
         }, 1500);
     });
 
@@ -34,19 +25,19 @@ export function Features({user, game}) {
                 <p>Feature suggestion description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut justo nisl. Sed aliquet tellus mi, sit amet aliquet diam suscipit tincidunt. Proin mollis feugiat libero ut dapibus. Aliquam erat volutpat. Maecenas a arcu vitae ipsum tristique condimentum quis id purus. Praesent lobortis odio in sem feugiat tempus sit amet id tortor. Praesent at dignissim justo, quis aliquet nulla. Etiam lobortis augue vitae sem gravida, vestibulum euismod eros suscipit. Vestibulum pretium odio quis erat convallis, in ullamcorper sapien mattis. Nunc efficitur ipsum nulla.</p>
 
                 <p>Integer est risus, iaculis id feugiat non, efficitur sit amet ipsum. Vestibulum sodales magna ipsum, nec sollicitudin elit mattis vel. Pellentesque hendrerit nec sem nec porta. Fusce pellentesque dui ex, ut faucibus orci sagittis congue. Nulla facilisi. Curabitur nisi nulla, pharetra non elit sit amet, molestie aliquam sem. Donec fermentum elementum enim. Suspendisse a molestie lorem, eu fermentum sapien. Pellentesque tincidunt est sapien, eget tristique felis condimentum et. Maecenas sit amet luctus ipsum. Vestibulum et tortor quis odio ultrices ornare vel vel libero. Phasellus iaculis lacus turpis, ut feugiat metus porta at.</p>
-                <p className="text-center text-secondary">(votes for this feature suggestion)</p>
+                <p className="text-center text-secondary">Upvotes</p>
             </div>
             
             <br />
 
-            <div>{msg2}</div>
+            <div>{msg}</div>
             <div className="alert alert-primary px-3 py-2">
                 <h3>Feature Suggestion Title 2</h3>
                 <p className="text-secondary">Author</p>
                 <p>Feature suggestion description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut justo nisl. Sed aliquet tellus mi, sit amet aliquet diam suscipit tincidunt. Proin mollis feugiat libero ut dapibus. Aliquam erat volutpat. Maecenas a arcu vitae ipsum tristique condimentum quis id purus. Praesent lobortis odio in sem feugiat tempus sit amet id tortor. Praesent at dignissim justo, quis aliquet nulla. Etiam lobortis augue vitae sem gravida, vestibulum euismod eros suscipit. Vestibulum pretium odio quis erat convallis, in ullamcorper sapien mattis. Nunc efficitur ipsum nulla.</p>
 
                 <p>Integer est risus, iaculis id feugiat non, efficitur sit amet ipsum. Vestibulum sodales magna ipsum, nec sollicitudin elit mattis vel. Pellentesque hendrerit nec sem nec porta. Fusce pellentesque dui ex, ut faucibus orci sagittis congue. Nulla facilisi. Curabitur nisi nulla, pharetra non elit sit amet, molestie aliquam sem. Donec fermentum elementum enim. Suspendisse a molestie lorem, eu fermentum sapien. Pellentesque tincidunt est sapien, eget tristique felis condimentum et. Maecenas sit amet luctus ipsum. Vestibulum et tortor quis odio ultrices ornare vel vel libero. Phasellus iaculis lacus turpis, ut feugiat metus porta at.</p>
-                <p className="text-center text-secondary">(votes for this feature suggestion)</p>
+                <p className="text-center text-secondary">Upvotes</p>
             </div>
             
             <br />
@@ -58,7 +49,7 @@ export function Features({user, game}) {
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Description:</label>
-                    <input type="text" placeholder="type here..." className="form-control py-5" />
+                    <textarea placeholder="type here..." className="form-control" wrap="soft"></textarea>
                 </div> 
                 <div className="d-flex justify-content-between align-items-end">
                     <button className="btn btn-info text-light">Submit Feature Suggestion</button>
