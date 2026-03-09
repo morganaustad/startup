1bcd2f9ae3db08f70eb6ef99da9ea795e187b26c
morganaustad <morganaustad@gmail.com>
2026-03-09 10:44:17 -0600

    Add unauthenticated.jsx

---
+++ src/login/unauthenticated.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import { MessageDialog } from './messageDialog';
+
+export function Unauthenticated(props) {
+  const [userName, setUserName] = React.useState(props.userName);
+  const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('user', userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
+  }
+
+  return (
+    <>
+      <main className="justify-content-center align-items-center">
+        <h2 className="text-center">Welcome to <span>FeatureReacher</span></h2>
+        <div className='px-4 py-3 border'>
+            <div className='mb-3'>
+                <label className='form-label'>Email:</label>
+                <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
+            </div>
+            <div className='mb-4'>
+                <label className='form-label'>Password:</label>
+                <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
+            </div>
+            <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
+            Login
+            </Button>
+            <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
+            Create
+            </Button>
+        </div>
+      </main>
+
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+    </>
+  );
+}
