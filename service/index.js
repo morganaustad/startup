1db5173ca4bee4e35d6f2716a0ced4f6f5ec5ad3
morganaustad <morganaustad@gmail.com>
2026-03-09 09:59:14 -0600

    Create create user and login user functions

--- service/index.js
+++ service/index.js
@@ -4,4 +4,38 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
+
+let users = [];
+let posts = [];
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createServerModuleRunner(req.body.email, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ email: user.email });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
