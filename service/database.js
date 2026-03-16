f150ddd68a89b0df58eb3a646249df9cb273ad0c
morganaustad <morganaustad@gmail.com>
2026-03-16 13:52:20 -0600

    Add database logic

---
+++ service/database.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const postCollection = db.collection('post');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
+}
+
+async function addPost(post) {
+  return postCollection.insertOne(post);
+}
+
+function getPosts() {
+  const options = {
+    sort: { created: -1 },
+  };
+  const cursor = postCollection.find({}, options);
+  return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  updateUserRemoveAuth,
+  addPost,
+  getPosts
+};
